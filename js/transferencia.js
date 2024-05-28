d2b97119a2a478e20a6fd4b8353cbb714a402a81
Bruno Silva <brunoapple010@hotmail.com>
2024-05-28 19:52:34 -0300

    Update transferencia.js

--- js/transferencia.js
+++ js/transferencia.js
@@ -2,10 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('.transfer-form');
 
     form.addEventListener('submit', function(event) {
-        // Evita o envio padrão do formulário
         event.preventDefault();
 
-        // Redireciona para a página de sucesso após 1 segundo
         setTimeout(function() {
             window.location.href = 'transfe_realiza.html';
         }, 1000);
